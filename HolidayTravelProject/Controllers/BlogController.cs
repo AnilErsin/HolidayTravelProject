dbd6eddf1b4465b1873cd89ba720cd2105c41928
AnilErsin <anil_ersin61@hotmail.com>
2023-04-13 16:50:25 +0300

    blogYorum sınıfını oluşturduk, bloglara ait içerikleri blogYorum sınıfımızdan çektik

--- HolidayTravelProject/Controllers/BlogController.cs
+++ HolidayTravelProject/Controllers/BlogController.cs
@@ -18,11 +18,16 @@ namespace HolidayTravelProject.Controllers
             return View(blogs);
         }
 
+        BlogYorum by = new BlogYorum();
         public ActionResult BlogDetay(int id)
         {
-            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            
 
-            return View(blogbul);
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+
+            return View(by);
         }
     }
 }
