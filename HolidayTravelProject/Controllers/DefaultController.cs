7176249fcd5246cd22005ea9e336bffacd1dfe9b
AnilErsin <anil_ersin61@hotmail.com>
2023-04-15 17:27:43 +0300

    ana sayfa slider veritabanından çekildi

--- HolidayTravelProject/Controllers/DefaultController.cs
+++ HolidayTravelProject/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HolidayTravelProject.Models.Sınıflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,11 @@ namespace HolidayTravelProject.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
 
         public ActionResult About()
