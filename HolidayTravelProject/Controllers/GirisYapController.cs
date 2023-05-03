98e3a56e8274d152badb695fcb8ffc293a93a7e8
AnilErsin <anil_ersin61@hotmail.com>
2023-05-03 19:07:20 +0300

    Login İşlemi session ve cokkie üzerinden eklendi

--- HolidayTravelProject/Controllers/GirisYapController.cs
+++ HolidayTravelProject/Controllers/GirisYapController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using HolidayTravelProject.Models.Sınıflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace HolidayTravelProject.Controllers
 {
     public class GirisYapController : Controller
     {
+        Context c = new Context();
         // GET: GirisYap
         public ActionResult Index()
         {
@@ -16,6 +19,22 @@ namespace HolidayTravelProject.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin ad)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
+
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
